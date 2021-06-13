642acf506db7c45c8f8886f303a63bfaf7c99c76
Ezequiel Tavares <eot_ccb@hotmail.com>
2021-06-12 21:38:30 -0300

    Change variables Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   URL: 'http://localhost/curso_dev/api.php',
   firebaseConfig : {
-    apiKey: "AIzaSyBYaUE9e7_qfy0Khz9Q9dwT35yEMeuyxM4",
-    authDomain: "cashon-2021.firebaseapp.com",
-    projectId: "cashon-2021",
-    storageBucket: "cashon-2021.appspot.com",
-    messagingSenderId: "639826885407",
-    appId: "1:639826885407:web:a1159e64d67e7d9d428e2a",
-    measurementId: "G-4Z96FHN2Y7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
